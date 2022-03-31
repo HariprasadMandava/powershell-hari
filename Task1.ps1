cbb6aac79c79634970dd519b5fc1f2c765223798
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-03-31 19:33:55 +0530

    Add files via upload

---
+++ Task1.ps1
@@ -0,0 +1,10 @@
+﻿Get-Service 
+Get-Service | Where-Object { $_.status -eq “Running”}
+$Service= "ALG"
+if ($Service.Status -eq "Running") {
+    Write-Host "Running"
+    } elseif ($Service.Status -eq "Stopped") {
+    Start-Service $Service
+    }
+    $a=Get-Service ALG
+    $a
---
+++ Task3.ps1
@@ -0,0 +1,4 @@
+﻿$now=Get-Date
+$path="Task3"+$now.ToString("dd-MM-yyyy---hh-mm-ss") 
+Get-Process | Sort-Object -Property Handles -Descending | Select-Object -First 10 | Out-File .\Desktop\$path.txt
+
