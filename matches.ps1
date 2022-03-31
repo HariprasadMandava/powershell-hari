6a04e9c34fd21af43b96b0e36de376c699def2c6
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-03-31 19:32:05 +0530

    Add files via upload

---
+++ matches.ps1
@@ -0,0 +1,14 @@
+﻿$a= Read-Host -Prompt "Enter a value: "
+if($a -eq 10){
+Write-Host $a "Given value matches with 10"
+}
+elseif($a -eq 30){
+Write-Host $a "Given value matches with 30"
+}
+elseif($a -eq 1000)
+{
+Write-Host $a "Given value matches with 1000"
+}
+else{
+Write-Host "Given value does not match with required values"
+}
