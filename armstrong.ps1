aa51983fedfc7237403494f0d8499a8f01b0d5b9
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-03-31 21:57:51 +0530

    Add files via upload

---
+++ armstrong.ps1
@@ -0,0 +1,13 @@
+﻿$number = Read-Host -Prompt "Enter a Number"
+$sum = 0;
+$temp = $number;
+$r = 1;
+while ($number -gt 0) {
+$r =[int] $number%10;
+$sum =[int] $sum+($r*$r*$r);
+$number = [math]::Floor($number/10);
+}
+if($temp -eq $sum){
+Write-Host "Armstrong Number"
+}
+
