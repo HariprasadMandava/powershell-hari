a94c872dcf3b214c336ecbe34976a31e9144f136
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-03-31 22:02:55 +0530

    Add files via upload

---
+++ Natural.ps1
@@ -0,0 +1,11 @@
+﻿$number = Read-Host -Prompt "Enter a Number: "
+function get-feb ($number) {
+$a = 1 ;
+$sum = 0;
+while ($a -le $number) {
+$sum = $sum+$a;
+$a = $a+1
+}
+$sum
+}
+get-feb ($number)
