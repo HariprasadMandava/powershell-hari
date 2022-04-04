dbe3e12f529b6b2611965cb9ffbc06b7217f4504
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-04 12:00:20 +0000

    Add files via upload

---
+++ NullLiterals.ps1
@@ -0,0 +1,15 @@
+﻿<#NULL LITERALS
+
+Null:- Is used to represent null values.
+
+#>
+
+
+
+$null -eq $undefinedvariable
+
+function Get_Nothing{}
+$value = Get_Nothing
+$null -eq $value
+Get_Nothing
+
