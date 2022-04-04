42b623765105cf0c430968d2c92378feffdc7488
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-04 06:03:06 +0000

    Add files via upload

---
+++ MultiplierSuffixes.ps1
@@ -0,0 +1,14 @@
+﻿<# MULTIPLIER SUFFIXES
+Suffixes:-      Multipliers:-
+kb              1kb
+mb              1mb
+gb              1gb
+tb              1tb
+pb              1p
+#>
+
+1kb
+1mb
+1gb
+1tb
+1pb
---
+++ NumericLiterals.ps1
@@ -0,0 +1,18 @@
+﻿<#INTEGER LITERALS
+--Numbers that do not have a decimal point or Exponential point
+Decimal:-Digits in between 0-9 & not to "0".
+Hexa-Decimal:-Digits are with 0,x,X followed by any 0-9.
+Octa-Decimal:-Starts with "0" & in between 0-7
+#>
+
+
+48576
+
+0x3b24
+0Xf96
+0x21
+
+
+0
+0125
+0324
