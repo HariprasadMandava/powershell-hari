aa29b06ab30fd897df57d62e17e44e54ba0805f7
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-04 11:59:58 +0000

    Add files via upload

---
+++ HashLiterals.ps1
@@ -0,0 +1,10 @@
+﻿<#HASH
+-->Hash literal is used to create a hash table of zero or more elements
+   each of which is a key/value pair.
+#>
+
+
+$h1 = @{firstname="Hari";lastname="Prasad";ID = 1} 
+
+
+$h1
