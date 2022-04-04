665dc39147ce358ad990f183ad611bae313dbcbe
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-04 06:02:32 +0000

    Add files via upload

---
+++ StringLiterals.ps1
@@ -0,0 +1,15 @@
+﻿<#STRING LITERALS
+--Sequence of characters from the source character set enclosed in (" " )
+#>
+
+
+#Single-Quotation
+$PS = 'This is Powershell'
+$PS
+#Double-Quotation
+$PS = "This is Powershell"
+$PS
+#Concatination
+$P = "power"
+$s = "shell"
+$p+$s
