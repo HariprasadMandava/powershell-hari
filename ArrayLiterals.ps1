eb569e7e40e6a736177d93288bbeb73b39cc24e1
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-04 11:59:01 +0000

    Add files via upload

---
+++ ArrayLiterals.ps1
@@ -0,0 +1,10 @@
+﻿<#ARRAY LITERALS
+Array:- There are no array literals in powershell JArray are built using(Operator & Caste).
+#>
+
+$a = ,1
+$b = 1,2
+$c = 1,2,3
+$d = [array]1
+$e = (1,2),(3,4)
+$f = ,(5,6)
