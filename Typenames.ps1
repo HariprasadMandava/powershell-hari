236cf74a299dfd4c3ebc1a9ddfd3f36d6020e6db
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-05 07:30:52 +0000

    Add files via upload

---
+++ Typenames.ps1
@@ -0,0 +1,13 @@
+﻿<#TYPE NAMES
+.PSTypeNames
+.PSObject
+.HashTable
+#>
+
+#PSTypeNames
+$alpha = [PSCustomObject]@{a=1; b=2}
+$alpha.pstypenames.Clear()
+$alpha.Pstypenames.Add('HARI')
+$alpha|Get-Member
+
+#PSObject
