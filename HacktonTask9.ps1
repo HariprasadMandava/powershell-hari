b7b090e66a0de150c872d2afc5925d7b802af47e
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-06 10:49:12 +0000

    Add files via upload

---
+++ HacktonTask9.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "mandava Hariprasad"
+$s = Get-WmiObject -Class Win32_BIOS -computerName dc1,aiqum,jumphost | Select-Object SMBIOSBIOSVersion,SerialNumber,PSComputerName
+$s | Out-GridView
