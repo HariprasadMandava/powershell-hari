df52bbc0b91e6335265d0319d5d333be58a354c5
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-06 06:04:00 +0000

    Add files via upload

---
+++ HackthonTask6.ps1
@@ -0,0 +1,6 @@
+﻿ Write-Host "Mandava Hariprasad"
+ $Session = New-PSSession -ComputerName "dc1" -Credential "DEMO\Administrator"
+ $Session
+ $source = "C:\Users\Administrator.Demo\Desktop\Servernames.txt"
+ Copy-Item $source  -Destination "C:\Users\Administrator.Demo\Desktop\Demo\Servernames.txt" -ToSession $Session 
+
