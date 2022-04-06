58e8313a844dc9d3a1677fb916340f3951e13744
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-06 06:04:21 +0000

    Add files via upload

---
+++ HacktonTask8.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "Hariprasad Mandava"
+$p = Get-Process -ComputerName dc1,aiqum | Sort-Object CPU -Descending | Select -first 10| Select ProcessName,ID,CPU,MachineName | ConvertTo-Html
+$p 
