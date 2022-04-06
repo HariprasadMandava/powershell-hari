ee0263bb321b985f7528bb8b486a514548d1a619
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-06 08:43:37 +0000

    Add files via upload

---
+++ HackthonTask10.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Hariprasad Mandava"
+$s = Get-Content "C:\Users\Administrator.DEMO\Desktop\Servernames.txt"
+foreach($a in $s){
+New-PSSession -ComputerName $a
+Enter-PSSession -ComputerName $a  -Credential DEMO\Administrator
+Get-wmiObject -Class win32_bios -ComputerName $a | Select-Object PSComputerName
+Exit-PSSession
+}
