4810b41301848c2430693dd477819890405ea5fa
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-06 11:15:06 +0000

    Add files via upload

---
+++ HacktonTask11.ps1
@@ -0,0 +1,16 @@
+﻿$a = Read-Host ("Enter Command")
+@"
+1-full
+2-example
+3-online
+4-parameter
+5-detailed
+"@
+$b = Read-Host "read"
+switch($b){
+1{get-help($a) -full}
+2{get-help($a) -example}
+3{get-help($a) -online}
+4{get-help($a) -parameter * }
+5{get-help($a) -detailed }
+}
