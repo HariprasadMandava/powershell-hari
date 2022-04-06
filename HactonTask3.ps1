730f06b3684f9390ca759e3f941f2316c51345b1
HariprasadMandava <100347569+HariprasadMandava@users.noreply.github.com>
2022-04-06 10:42:32 +0000

    Add files via upload

--- HactonTask3.ps1
+++ HactonTask3.ps1
@@ -1,12 +1,13 @@
-﻿Import-Module ActiveDirectory
-
-$firstname = Read-Host -Prompt "Please Enter The First Name"
-$lastname = Read-Host -Prompt "Please Enter The Last Name"
-New-ADUser ` 
-     -Name "$firstname $lastname" `
-     -GivenName $firstname `
-     -Surname $lastname `
-     -UserPrincipalName "$firstname.$lastname" `
-     -AccountPassword (ConvertTo-SecureString "P@ssword123" -AsPlainText -force) `
-     -path ""
-     ChangePasswordAtLogon 1
+﻿I  Write-Host "Hariprasad Mandava" 
+Import-Module ActiveDirectory   
+$firstname = Read-Host -Prompt "Please Enter The First Name" 
+$lastname = Read-Host -Prompt "Please Enter The Last Name" 
+New-ADUser `           
+    -Name "$firstname $lastname" `          
+    -GivenName $firstname `         
+    -Surname $lastname `          
+    -UserPrincipalName "$firstname.$lastname" `          
+    -AccountPassword (ConvertTo-SecureString "P@ssword123" -AsPlainText -force) `          
+    -path "CN=Users,DC=demo,DC=netapp,DC=com"          
+    -ChangePasswordAtLogon 1 `     
+    -Enabled 1 
